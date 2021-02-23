efd2f7c06b958938a48956d806eade6035238e09
Ksh <kshcodding@gmail.com>
2021-02-23 22:25:22 +0100

    Created main function to match with FizzBuzzGame

--- FizzBuzz/Program.cs
+++ FizzBuzz/Program.cs
@@ -10,7 +10,9 @@ namespace FizzBuzz
     {
         static void Main(string[] args)
         {
-           
+            Console.WriteLine("Alice, choisis un nombre pour Bob : ");
+            var chosedNumber = int.Parse(Console.ReadLine());
+            FizzBuzzGame.AliceGiveNumberToBob(chosedNumber);
         }
     }
 }
